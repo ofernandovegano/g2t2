130e272fde20973a57f01914ebb1e801067f15a8
Fernando Gomes <gomesrodriguesfernando@gmail.com>
2021-06-02 01:38:01 -0300

    setup backend

---
+++ src/server.js
@@ -0,0 +1,9 @@
+import app from './app';
+import express from 'express'
+
+app.use(express.json())
+const port = process.env.PORT || 5000
+app.listen(port, error => {
+  if (error) throw error;
+  console.log('server running on port ' + port);
+});
