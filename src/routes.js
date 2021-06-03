48754bf1e94ebf9be166a40eeac92b8660112785
Fernando Gomes <gomesrodriguesfernando@gmail.com>
2021-06-03 19:23:47 -0300

    refactored to ES5

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,17 @@
-import express  from 'express';
+const express  = require ('express');
 const router = express.Router();
 
-import app from './app'
-import path from 'path';
+const app = require ('./app')
+const path = require ('path');
 
 
 // Conection between backend and frontend
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   })
 };
 
-export default router;
+module.exports = router;
