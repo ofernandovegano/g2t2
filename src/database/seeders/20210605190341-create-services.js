00171bdf76beafc75d9fcf2968243bdeb114cb9c
andreludwig <andre@roadseg.com.br>
2021-06-05 16:12:28 -0300

    creating services seed

---
+++ src/database/seeders/20210605190341-create-services.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert("services", [
+      {
+        date_schedule: new Date(),
+        date_service: new Date(),
+        time_service: new Date(),
+        price: "150",
+        status_service: "Agendado",
+        client_id: "2",
+        specialist_id: "3",
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        date_schedule: new Date(),
+        date_service: new Date(),
+        time_service: new Date(),
+        price: "80",
+        status_service: "Realizado",
+        id_cliente: "1",
+        specialist_id: "1",
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    ]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete("services");
+  }
+};
