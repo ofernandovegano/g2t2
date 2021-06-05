1f817463288d89187741550b5461f8042d944c6c
andreludwig <andre@roadseg.com.br>
2021-06-05 15:03:31 -0300

    creating services migration

---
+++ src/database/migrations/20210605175840-create-services.js
@@ -0,0 +1,58 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable("services", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      date_schedule: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      date_service: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      time_service: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      price: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+      },
+      status_service: {
+        type: Sequelize.ENUM("AGENDADO", "REALIZADO", "CANCELADO"),
+        allowNull: false,
+      },
+      client_id: {
+        type: Sequelize.INTEGER,
+        references: { model: "clients", key: "id" },
+        onUpdate: "CASCADE",
+        onDelete: "SET NULL",
+      },
+      specialist_id: {
+        type: Sequelize.INTEGER,
+        references: { model: "specialists", key: "id" },
+        onUpdate: "CASCADE",
+        onDelete: "SET NULL",
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("services");
+  },
+};
