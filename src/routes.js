e2bacbdc5fca0994e93ac2dad523da9c18217cf1
Ariele Mendonça <ariele_apm@outlook.com>
2021-06-21 21:43:41 -0300

    Add cors

--- src/routes.js
+++ src/routes.js
@@ -18,7 +18,10 @@ import CheckinController from './app/controllers/CheckinController'
 
 
 routes.get('/', (req, res, next) => {
-  // res.send({message: 'Hello World'})
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
   next()
 })
 
