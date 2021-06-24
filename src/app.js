90976f230e556e3a9cd58ea4dda63b930a801265
Ariele Mendonça <ariele_apm@outlook.com>
2021-06-24 20:57:30 -0300

    Fix sucessStatus

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ class App{
   }
   
   middleware() {
-    this.server.use(cors({ origin: "*", optionsSuccessStatus: "*", allowedHeaders: "*"}));
+    this.server.use(cors({ origin: "*", optionsSuccessStatus: 200, allowedHeaders: "*", credentials: true}));
     this.server.use(express.json());
     this.server.use(express.urlencoded({
       extended: true
